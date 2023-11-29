7f7f58d69197dac3990d0c35fd97758bd3720e12
Bárbara Sousa <barbaravieirasousa@gmail.com>
2023-11-29 18:10:13 +0000

    feat: Add comments to code and new method to find location

--- src/main/java/com/assignments/customerservice/domain/repository/CustomerRepository.java
+++ src/main/java/com/assignments/customerservice/domain/repository/CustomerRepository.java
@@ -5,7 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends JpaRepository <Customer, Integer> {
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
+    /**
+     * Query to find the customer given a certain customer reference.
+     *
+     * @param customerRef is a unique identifier for the customer.
+     * @return customer entity.
+     */
     Customer findByCustomerRef(String customerRef);
 }
--- src/main/java/com/assignments/customerservice/domain/repository/LocationRepository.java
+++ src/main/java/com/assignments/customerservice/domain/repository/LocationRepository.java
@@ -5,6 +5,16 @@ import com.assignments.customerservice.domain.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface LocationRepository extends JpaRepository <Location, Integer> {
+public interface LocationRepository extends JpaRepository<Location, Integer> {
+
+    /**
+     * Query to find a location given a certain postcode.
+     *
+     * @param postcode is a unique identifier for the location.
+     * @return the location for the given postcode.
+     */
+    Optional<Location> findByPostcode(String postcode);
 }
